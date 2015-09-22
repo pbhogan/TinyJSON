4734c622bf7a3635cbbd0e24cfd3eb51250e7b9d
Patrick Hogan <pbhogan@gmail.com>
2015-09-22 10:11:27 -0500

    ...

--- TinyJSON/JSON.cs
+++ TinyJSON/JSON.cs
@@ -36,6 +36,9 @@ namespace TinyJSON
 	}
 
 
+	/// <summary>
+	/// Mark members to force type hinting even when EncodeOptions.NoTypeHints is set.
+	/// </summary>
 	[AttributeUsage( AttributeTargets.Field | AttributeTargets.Property )]
 	public class TypeHint : Attribute
 	{
